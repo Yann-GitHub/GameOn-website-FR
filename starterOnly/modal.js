6b28a23f25c3aaad8b45f50efad59129559c8765
Yann <ybarlet@gmail.com>
2022-05-04 08:45:31 +0200

    Add comments and code refactoring

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,3 +1,6 @@
+
+// Injecte une class '.responsive' à l'élément '.myTopnav'. La fonction est appelée via le HTML au clic sur le menu hamburger
+// Permet de toggle le menu avec un clic sur l'icone hamburger - le display des éléments passe de 'none' à 'block'
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -13,9 +16,13 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
 // launch modal event
+// ajoute un event listener sur les 2 bouttons de la hero section (boutton visible en fonction de la résolution - media query)
+// la fonction callback est appelée au click et permet d'afficher la modal qui change son display 'none' en display 'block'
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
+// fonction intégré en callback de l'event listener sur les bouttons avec la class '.modal-btn'
+// elle modifie le display de la modal (none -> block) ce qui la fait apparaitre
 function launchModal() {
   modalbg.style.display = "block";
 }
