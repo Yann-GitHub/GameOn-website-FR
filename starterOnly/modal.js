fa614e66d01ade8812ebfb630b57dabc2e439d6b
Yann <ybarlet@gmail.com>
2022-05-11 09:58:42 +0200

    Add input validation with Regex

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,10 +11,17 @@ function editNav() {
 }
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
-const modalBtnCross = document.querySelector(".close"); // Récupère l'icone permettant la fermeture de la modale
+const modalbg = document.querySelector(".bground"); // Selectionne la div parent du formulaire
+const modalBtn = document.querySelectorAll(".modal-btn"); // Selectionne les 2 bouttons qui ouvrent la modal
+const formData = document.querySelectorAll(".formData"); // Selectionne les différentes sections du formulaire
+const modalBtnCross = document.querySelector(".close"); // Selectionne l'icone permettant la fermeture de la modale
+const firstName = document.querySelector("#first"); // Selectionne l'input first name
+const lastName = document.querySelector("#last"); // Selectionne l'input last name
+const mail = document.querySelector("#email"); // Selectionne l'input Email
+const birthdate = document.querySelector("#birthdate"); // Selectionne l'input birthdate
+const quantity = document.querySelector("#quantity"); // Selectionne l'input quantity - Participation tournois
+
+
 
 // launch modal event
 // ajoute un event listener sur les 2 bouttons de la hero section (boutton visible en fonction de la résolution - media query)
@@ -30,9 +37,62 @@ function launchModal() {
 
 // close modal form
 // ajout d'un event listener sur la croix qui permet la fermeture de la modale
-// au click la fonction callback est appelée et permet de fermer la modal en changant son display 'block' en display 'none'
+// au click la fonction callback est appelée et permet de fermer la modal en changant son display 'block' en display 'none'.
 modalBtnCross.addEventListener("click", closeModal);
 function closeModal() {
   modalbg.style.display = 'none';
 }
 
+
+
+// Gestion input first (prénom)
+firstName.addEventListener("input", function(event) {
+  event.preventDefault();
+  if(/^([\u00C0-\u017Fa-zA-Z]{1,}(?:[-]?[\u00C0-\u017Fa-zA-Z]{1,}))$/.test(event.target.value)) {
+    formData[0].setAttribute("data-error-visible", false);
+    formData[0].removeAttribute("data-error");
+  } else {
+    formData[0].setAttribute("data-error-visible", true);
+    formData[0].setAttribute("data-error", "Veuillez entrer 2 caractères ou plus pour le champ du prénom.");
+    console.log('error input prénom');
+  };
+})
+
+// Gestion input last (nom)
+lastName.addEventListener("input", function(event) {
+  event.preventDefault();
+  if(/^([\u00C0-\u017Fa-zA-Z]{1,}(?:[-]?[\u00C0-\u017Fa-zA-Z]{1,}))$/.test(event.target.value)) {
+    formData[1].setAttribute("data-error-visible", false);
+    formData[1].removeAttribute("data-error");
+  } else {
+    formData[1].setAttribute("data-error-visible", true);
+    formData[1].setAttribute("data-error", "Veuillez entrer 2 caractères ou plus pour le champ du prénom.");
+    console.log('error input nom');
+  };
+})
+
+// Gestion input email
+mail.addEventListener("input", function(event) {
+  event.preventDefault();
+  if(/^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/gim.test(event.target.value)) {
+    formData[2].setAttribute("data-error-visible", false);
+    formData[2].removeAttribute("data-error");
+  } else {
+    formData[2].setAttribute("data-error-visible", true);
+    formData[2].setAttribute("data-error", "Veuillez enter une adresse mail valide.");
+    console.log('error input email');
+  };
+})
+
+// Gestion input number (nombre de tournois)
+quantity.addEventListener("input", function(event) {
+  event.preventDefault();
+  if(/\b([0-9]|[1-9][0-9])\b/.test(event.target.value)) {
+    formData[4].setAttribute("data-error-visible", false);
+    formData[4].removeAttribute("data-error");
+  } else {
+    formData[4].setAttribute("data-error-visible", true);
+    formData[4].setAttribute("data-error", "Vous devez entrer un nombre entre 0 et 99");
+    console.log('error input nombre tournois');
+  };
+})
